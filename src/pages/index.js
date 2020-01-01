8771ee880f183bee1b97971a0019247a4f6d1023
janel lucia <thejanellucia@gmail.com>
2020-01-01 10:49:38 -0500

    updated about copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = (props) => (
     <div className="wrapper">
       <section className="landing-about">
         <div className="about-wrap">
-          <p>Hi, I'm a UX/UI Designer in Toronto crafting meaningful and strategic design with a clear voice and vision. I specialize in human centered design, brand identity &amp; graphic design.</p>
+          <p>Hi, I'm a UX/UI Designer in Toronto crafting meaningful and strategic design with a clear voice and vision. I specialize in human-centred design, brand identity &amp; graphic design.</p>
         </div>
       </section>
       <section className="portfolio">
@@ -35,7 +35,7 @@ const IndexPage = (props) => (
           <Img fluid={props.data.imageFive.childImageSharp.fluid} alt="sneek peek of this project" />
           <div className="home-project-info daisy-info">
             <h3>Pace yourself yoga by Daisy</h3>
-            <p className="category">— Branding, Web Design</p>
+            <p className="category">— Branding, Graphic Design</p>
           </div>
         </Link>
         <Link to="/livie-jewelry/" className="project-wrap">
@@ -78,7 +78,7 @@ const IndexPage = (props) => (
       </section>
       <section className="process">
         <div className="step-title">
-          <p>My work is rooted in research, collaboration and feedback. I stay curious, use history as my superpower, and I obsess over small details. Here's a quick example of my process:</p>
+          <p>My work is rooted in research, collaboration and feedback. I stay curious, use history as my superpower, and obsess over small details. Here's an example of my work flow:</p>
         </div>
         <div className="step step-1">
           <h3>Define the challenges —</h3>
@@ -86,15 +86,15 @@ const IndexPage = (props) => (
         </div>
         <div className="step step-2">
           <h3>Research &amp; Validate —</h3>
-          <p>Next I jump right into quantitative and qualitative research by conducting user interviews, surveys, industry research, and competitive analysis and use that information to build user personas and journeys.</p>
+          <p>Then, I dive deeper with quantitative and qualitative research; user interviews, surveys, industry research, and competitive analysis and use that info to build personas and journey maps.</p>
         </div>
         <div className="step step-3">
           <h3>Sketch, Vote &amp; Storyboard —</h3>
-          <p>Using the data gathered I design multiple solutions, facilitate a structured critique with stakeholders and use dot voting to select the best solution. From there I take the winning idea and turn it into a user storyboard.</p>
+          <p>Design/sketch multiple solutions, facilitate a critique session with stakeholders and use dot voting to select the best solution. Then, take the winning idea and turn it into a user storyboard.</p>
         </div>
         <div className="step step-5">
           <h3>Prototype &amp; Test —</h3>
-          <p>From there I create an active prototype, giving the team an opportunity to showcase product features and animations for user testing. This allows us to observe user behaviour for better functionality and usability.</p>
+          <p>With the winning design, create an active prototype, showcase product features and animations for user testing. This allows us to observe user behaviour for better functionality and usability.</p>
         </div>
         <div className="step step-6">
           <h3>Evaluate &amp; Iterate —</h3>
