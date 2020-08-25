39ef3b6856e9bc7dff6548ca78342694eea8d13a
janel lucia <thejanellucia@gmail.com>
2020-08-25 17:47:38 -0400

    services page edits

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -3,7 +3,7 @@ import Slider from 'react-animated-slider';
 import 'react-animated-slider/build/horizontal.css';
 
 const slides = [
-  // { name: 'Faith @ ATRIA COACHING', testimonial: "Lorem ipsum" },
+  { name: 'Tati @ MoonSign Supplies', testimonial: "Working with Janel has been amazing! I can’t recommend her enough. She was able to help problem-solve and trouble-shoot a lot of issues I was facing when developing my Services page. She is such a pleasure to work with. I definitely will be working with her again!" },
   { name: 'Taylor @ GIRLS20', testimonial: "Our organization was looking for a designer to help us transform our website and Janel delivered an exceptional service and continues to offer assistance and advice. Janel is a fantastic graphic designer and web developer and always delivers above and beyond." },
   { name: 'Marcel @ J.R & Associates', testimonial: "Janel was able to understand everything that was important to my business and designed a comprehensive website with a style that represents our brand.  Janel made the process so easy. She will be my first call for all my web and brand needs." },
   { name: 'Oliveira @ Kids Wish Network', testimonial: "Janel is an exceptional UX professional. She has the right balance between big picture thinking and attention to detail." },
--- src/pages/services.js
+++ src/pages/services.js
@@ -50,65 +50,76 @@ const Services = (props) => (
               <input type="checkbox" id="chck1" />
               <label class="tab-label" for="chck1">Brand Values</label>
               <div class="tab-content">
-                <p>Brand values are a set of guiding principles that shape every aspect of your business. They’re placed at the very core of your brand and are there to dictate your brand message, identity and personality. They affect the choices you make and the actions you take and are the main reason customers will connect with you over the competition. Investing in your brand is crucial if you want your business to be unique.</p>
+                <p>Brand values are the foundations that shape every aspect of your brand. They’re placed at the very core of decision making and are there to dictate your brand message, identity and personality. They inform the choices you make and the actions you take and are the main reason customers will connect with your brand.</p>
                 <ul>
-                  <li><span className="bold">User Persona(s)</span> — define your audience, who are you talking to?</li>
-                  <li><span className="bold">Values</span> — define a set of 5+ values that will help create a consistent brand across all platforms.</li>
+                  <li><span className="bold">User Persona(s)</span> — Define your audience, who are you talking to?</li>
+                  <li><span className="bold">Design Principles</span> — Define a set of 5+ values that will help create a consistent brand across all platforms.</li>
                   <li><span className="bold">Positioning</span> — Defining your brand positioning is very similar to creating a mission statement.</li>
                 </ul>
-                <p>We refer to this throughout the design process and beyond to ensure clear and informed design and decisions throughout. Defining your values is the core to all of my services, I believe in creating informed design.</p>
+                <p>We refer to these values throughout the design process and beyond to ensure clear and informed design and decisions. Defining your values is the core to all of my services, I believe in creating informed design with a clear message and goal.</p>
+                <a href="mailto:hello@janellucia?subject=Building Design Values" className="get-started-link">Get Started Today</a>
               </div>
             </div>
             <div className="tab">
               <input type="checkbox" id="chck2" />
               <label class="tab-label" for="chck2">Brand Identity</label>
               <div class="tab-content">
-                <p>Brand identity is the face of a brand. While brand values are the emotional and even philosophical concepts, brand identity is the visual component of a brand that represents those larger ideas.</p>
-                <p>Brand identity includes logos, typography, colors, packaging, and messaging, and it complements and reinforces the existing reputation of a brand. Brand identity attracts new customers to a brand while making existing customers feel at home. It's both outward- and inward-facing.</p>
-                <p>It's vital that brand identity be consistent. Because it's representing and reinforcing the emotions of a brand, the message portrayed by brand identity components needs to be clear, and it needs to be the same no matter where it's displayed.</p>
-                <p>To manage brand identity, organizations should invest in a brand identity that helps them stay consistent while still having the flexibility and the speed necessary to succeed in today's market.</p>
+                {/* <p>Brand identity is the face of a brand. While brand values are the emotional and even philosophical concepts, brand identity is the visual component of a brand that represents those larger ideas.</p> */}
+                <p>Brand identity includes logos, typography, colors, packaging, and messaging, and it complements and establishes or reinforces the reputation of a brand. Brand identity attracts new customers to a brand while making existing customers feel at home. It's both outward- and inward-facing.</p>
+                <p>It's vital that brand identity be consistent because it's representing and reinforcing the emotions of a brand, the message portrayed by brand identity components needs to be clear, and it needs to be the same no matter where it's displayed.</p>
+                <p>Invest in a brand identity that helps you stay consistent while still having the flexibility and speed necessary to succeed in today's market.</p>
+                <a href="mailto:hello@janellucia?subject=Building a Brand Identity" className="get-started-link">Get Started Today</a>
               </div>
             </div>
             <div className="tab">
               <input type="checkbox" id="chck3" />
               <label class="tab-label" for="chck3">User Experience Design</label>
               <div class="tab-content">
-                <p>Delivering a complete digital experience is like glue binding together all touch points, connecting devices, and creating omni-channel experiences. Using best practices and techniques, with a thorough understanding of all the latest technology tools, my UX strategy can create solutions that are both intuitive to use, and tailored to match specific user tasks and business goals.</p>
-                <p>I always ensure that the end user is at the heart of design considerations, offering  a range of design services from UX research to interaction design, as well as full usability audits. My UX design approach is rooted in collaborative and iterative design, coupled with an open communication approach. I believe that the key to success rests upon data-driven user insights, clearly defined goals, and solid validation.</p>
+                <p>Using best practices and techniques, with a thorough understanding of all the latest technology tools, my UX strategy can create solutions that are both intuitive to use, and tailored to match specific user tasks and business goals.</p>
+                <p>I ensure that the end user is at the heart of design considerations, offering  a range of design services from UX research to interaction design, as well as full usability audits. </p><p>My UX design approach is rooted in collaborative and iterative design, coupled with an open communication approach. I believe that the key to success rests upon data-driven user insights, clearly defined goals, and solid validation.</p>
+                <a href="mailto:hello@janellucia?subject=User Experience Design Inquiry" className="get-started-link">Get Started Today</a>
               </div>
             </div>
             <div className="tab">
               <input type="checkbox" id="chck4" />
               <label class="tab-label" for="chck4">Site Design</label>
               <div class="tab-content">
-                <p>Your site is where your audience should feel connected to your brand — that you understand their needs, and that they've finally found the solutions they've been looking for.</p>
-                <p>Good site design does not only mean the website looks good. It has to make sure that the website is functional, easy to navigate, users find what they are looking for and key elements like Call-to-actions (CTA) are prominently placed. Use of clear crisp images, text spacing, color scheme, typography, accessibility are all elements in a good website design.</p>
-                <p>A successful site can look different for every business, and that's why it's important to make sure your message is coming through crystal clear.</p>
+                <p>Your site is where your audience connects to your brand — where your brand connects to their needs, and where your audience (hopefully) gets that feeling that they finally found the solutions they've been looking for.</p>
+                <p>Successful design not only means it looks good, it has to be functional, easy to navigate so users find what they need. Use of clear crisp images, text spacing, color scheme, typography, accessibility are all elements in successful site design. Success looks different for every business, and that's why it's important to make sure your message is coming through crystal clear.</p>
+                <a href="mailto:hello@janellucia?subject=Website Design" className="get-started-link">Get Started Today</a>
               </div>
             </div>
             <div className="tab">
               <input type="checkbox" id="chck5" />
               <label class="tab-label" for="chck5">Site Build</label>
               <div class="tab-content">
-                <p>Get your website up and running, with options ranging from basic theme setup (Wordpress/Shopify/Squarespace) to completely custom solutions for your brand or business.</p>
-                <p>Whether you’re launching a site, need an ecommerce audit to learn how you can grow your business, are looking to migrate from one platform to another, or need a bespoke site with unique functionality, I will work with your team to achieve the results you need. With over 5 years experience building websites, I know my stuff when it comes to design and the development of competitive websites of the highest calibre.</p>
+                {/* <p>Get your website up and running, with options ranging from basic theme setup (Wordpress/Shopify/Squarespace) to completely custom web solutions, I will work with you to ensure your site acheives all your business goals.</p> */}
+                <p>Whether you’re launching a new site, need an site/ecommerce audit to learn how you can grow your business, are looking to migrate from one platform to another, or need a bespoke site with unique functionality, I will work with you to ensure your site acheives all your business goals and meets WCAG sccessibility standards.</p>
+                <ul>
+                  <li><span className="bold">Shopify</span> — Basic theme set up or fully custom theme with sections</li>
+                  <li><span className="bold">Squarespace</span> — Basic theme set up or fully custom theme</li>
+                  <li><span className="bold">Wordpress</span> — Basic theme set up or fully custom theme, woocommerce setup</li>
+                </ul>
+                <a href="mailto:hello@janellucia?subject=Website Build" className="get-started-link">Get Started Today</a>
               </div>
             </div>
             <div className="tab">
               <input type="checkbox" id="chck6" />
               <label class="tab-label" for="chck6">Design/Site Audit</label>
               <div class="tab-content">
-                <p>An audit comes with a complete and in-depth analysis of your website's conversion factors. Is your brand story clear and shining through? Do prospective customers know instantly what you can do for them? Is your necessary above-the-fold content maximizing its potential?</p>
+                <p>An audit is a complete and in-depth analysis of your website's conversion factors. Is your brand story clear and shining through? Do prospective customers know instantly what you can do for them? Is your necessary above-the-fold content maximizing its potential? Is your site accessible according to WCAG 2.0 standards?</p>
                 <p>I will analyze your site's first impressions on users, connect with potential customers who have never been exposed to your brand before, and get real insights — the make-or-break insights from the people that matter.</p>
                 <p>Detailed and actionable recommendations to improve your website immediately, as well as suggested long-term strategies for continued success, including content and promotional planning.</p>
+                <a href="mailto:hello@janellucia?subject=Website Audit" className="get-started-link">Get Started Today</a>
               </div>
             </div>
             <div className="tab">
               <input type="checkbox" id="chck7" />
               <label class="tab-label" for="chck7">Social Media Design</label>
               <div class="tab-content">
+                <p>Your customers are meeting each other, sharing ideas, and talking about your brand online. Give them a great first impression. I will work with you to create strong social media designs, we will take the look and feel of your website/brand to every social part of the web and get you interacting online in style.</p>
                 <p>Good social media marketing is all about high quality content and consistent branding. Neither is possible without proper design work, simply because social media is a visual platform for your company.</p>
-                <p>Your customers are meeting each other, sharing ideas, and talking about your brand online. Give them a great first impression. I will work with you to create strong social media designs, we will take the look and feel of your website/brand to every social part of the web and get you interacting with your customers.</p>
+                <a href="mailto:hello@janellucia?subject=Social Media Design" className="get-started-link">Get Started Today</a>
               </div>
             </div>
           </div>
