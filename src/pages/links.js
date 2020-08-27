6f9be7fc77066c5c4bb5466994482e0d93aa5abc
janel lucia <thejanellucia@gmail.com>
2020-08-26 20:39:12 -0400

    new link directory

---
+++ src/pages/links.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/layout'
+import Img from 'gatsby-image'
+import SEO from '../components/seo'
+
+import Testimonials from '../components/testimonials'
+
+const Links = (props) => (
+  <Layout name="about-page-wrap services-page-wrap">
+    <SEO title="Links" keywords={[` UX Designer`, `Product Designer`, `User Experience`, `User Research`, `Empathy`, `Compassion`]} />
+    <div className="wrapper">
+      {/* <section className="landing-about">
+        <div className="about-wrap">
+          <p>Hello, I design digital products and experiences that are beautifully simple, reassuringly smart and full of brand personality. Check out my services below &amp; let’s build something together.</p>
+        </div>
+      </section> */}
+      <section className="about-page">
+        <div className="about-wrap links-wrap">
+          <h2>@janellucia link directory</h2>
+          <p>Today and every day, I stand in solidarity with the movement for Black lives. I’ve curated these resources to help the community take action and make long-term commitments to anti-racism.</p>
+          <p>If you're looking to establish Diversity & Inclusion values into your business, check out my services and together we can define ethical, human standards at the core of your brand.</p>
+          <div className="tabs">
+            <div className="tab">
+              <a href="https://petitions.whitehouse.gov/petition/justice-breonna-taylor-5" target="new" class="insta-links">Justice for Breonna Taylor</a>
+            </div>
+            <div className="tab">
+              <a href="https://humanitariancoalition.akaraisin.com/ui/crisisinlebanonfundraising/participant/6117594?Lang=en-CA" target="new" class="insta-links">Donate to Canadians for Lebanon</a>
+            </div>
+            <div className="tab">
+              <a href="https://blacklivesmatter-canada.carrd.co/#english" target="new" class="insta-links">Black Lives Matter Canada — Sign Petitions/Protest/Donate</a>
+            </div>
+            <div className="tab">
+              <a href="https://docs.google.com/spreadsheets/d/1LHBfN_ceCMgBNYRMvFv_vw2NL8s72LRSUd9TYfIwZ24/edit#gid=0" target="new" class="insta-links">Anti-Racism Guide</a>
+            </div>
+            <div className="tab">
+              <a href="/" target="new" class="insta-links">janellucia.com</a>
+            </div>
+            <div className="tab">
+              <a href="/about" target="new" class="insta-links">About</a>
+            </div>
+            <div className="tab">
+              <a href="/services" target="new" class="insta-links">Services</a>
+            </div>
+            <div className="tab">
+              <a href="https://society6.com/letteringarchive" target="new" class="insta-links">Shop</a>
+            </div>
+            <div className="tab">
+              <a href="https://www.instagram.com/letteringarchive/" target="new" class="insta-links">@letteringarchive</a>
+            </div>
+            <div className="tab">
+              <a href="https://music.apple.com/profile/janellucia" target="new" class="insta-links">Apple Music Playlists</a>
+            </div>
+          </div>
+
+
+        </div>
+      </section>
+    </div>
+  </Layout>
+)
+
+export default Links
+
