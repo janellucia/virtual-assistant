e89e67bbcccdf1384d01032381b68c9d230846ab
janel lucia <thejanellucia@gmail.com>
2020-08-26 20:49:07 -0400

    new link directory

--- src/pages/links.js
+++ src/pages/links.js
@@ -8,7 +8,7 @@ import SEO from '../components/seo'
 import Testimonials from '../components/testimonials'
 
 const Links = (props) => (
-  <Layout name="about-page-wrap services-page-wrap">
+  <Layout name="about-page-wrap services-page-wrap links-page-wrap">
     <SEO title="Links" keywords={[` UX Designer`, `Product Designer`, `User Experience`, `User Research`, `Empathy`, `Compassion`]} />
     <div className="wrapper">
       {/* <section className="landing-about">
@@ -20,7 +20,7 @@ const Links = (props) => (
         <div className="about-wrap links-wrap">
           <h2>@janellucia link directory</h2>
           <p>Today and every day, I stand in solidarity with the movement for Black lives. I’ve curated these resources to help the community take action and make long-term commitments to anti-racism.</p>
-          <p>If you're looking to establish Diversity & Inclusion values into your business, check out my services and together we can define ethical, human standards at the core of your brand.</p>
+          <p>Establish diversity &amp; inclusion values into your business today, check out my service Brand Values and together we can define ethical, human standards at the core of your brands design, messaging &amp; beyond.</p>
           <div className="tabs">
             <div className="tab">
               <a href="https://petitions.whitehouse.gov/petition/justice-breonna-taylor-5" target="new" class="insta-links">Justice for Breonna Taylor</a>
@@ -35,13 +35,13 @@ const Links = (props) => (
               <a href="https://docs.google.com/spreadsheets/d/1LHBfN_ceCMgBNYRMvFv_vw2NL8s72LRSUd9TYfIwZ24/edit#gid=0" target="new" class="insta-links">Anti-Racism Guide</a>
             </div>
             <div className="tab">
-              <a href="/" target="new" class="insta-links">janellucia.com</a>
+              <a href="/" class="insta-links">janellucia.com</a>
             </div>
             <div className="tab">
-              <a href="/about" target="new" class="insta-links">About</a>
+              <a href="/about" class="insta-links">About</a>
             </div>
             <div className="tab">
-              <a href="/services" target="new" class="insta-links">Services</a>
+              <a href="/services" class="insta-links">Services</a>
             </div>
             <div className="tab">
               <a href="https://society6.com/letteringarchive" target="new" class="insta-links">Shop</a>
